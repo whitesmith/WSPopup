860dd189d6e39eeffd07f1d0dbc3d6f88fc52123
Ricardo Pereira <ricardopereira@whitesmith.co>
2019-02-06 11:08:05 +0000

    Change DEPLOYMENT_TARGET to 9

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,11 +11,26 @@ import WSPopup
 
 class ViewController: UIViewController {
 
+    lazy var showtimeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Showtime", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(showtimeButton)
+        showtimeButton.addTarget(self, action: #selector(self.showtimeTapped), for: .touchUpInside)
+
+        NSLayoutConstraint.activate([
+            showtimeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            showtimeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+        ])
     }
 
-    @IBAction func showtimeTapped(_ sender: Any) {
+    @objc func showtimeTapped(_ sender: Any) {
         let exampleSignInView = ExampleSignInView()
         exampleSignInView.userTextField.text = "JohnDoe123"
         let popupViewController = WSPopupViewController(popupView: exampleSignInView)
