4cd27be54d3717106f3904630446dff79d7ce0f1
Ricardo Pereira <ricardopereira@whitesmith.co>
2019-02-06 10:47:30 +0000

    Created framework

--- Example/ExampleSignInView.swift
+++ Example/ExampleSignInView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import WSPopup
 
 class ExampleSignInView: UIView, WSPopupActionable {
 
---
+++ WSPopup/WSPopup.h
@@ -0,0 +1,19 @@
+//
+//  WSPopup.h
+//  WSPopup
+//
+//  Created by Ricardo Pereira on 06/02/2019.
+//  Copyright © 2019 Whitesmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WSPopup.
+FOUNDATION_EXPORT double WSPopupVersionNumber;
+
+//! Project version string for WSPopup.
+FOUNDATION_EXPORT const unsigned char WSPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WSPopup/PublicHeader.h>
+
+
